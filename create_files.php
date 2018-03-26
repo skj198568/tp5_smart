92064ff6ccd583ffcd9ddf7fe2ba86f1c87f3de4
SongLeLe <skj198568@163.com>
2018-03-26 13:49:51 +0800

    FileController.php 文件不可直接覆盖

--- create_files.php
+++ create_files.php
@@ -83,6 +83,12 @@ foreach ($files as $file) {
             echo 'copy file: ' . $target_file . PHP_EOL;
             copy($file, $target_file);
         }
+    } else if (strpos($target_file, 'FileController') !== false) {
+        if (!is_file($target_file)) {
+            //覆盖文件
+            echo 'copy file: ' . $target_file . PHP_EOL;
+            copy($file, $target_file);
+        }
     } else {
         //覆盖文件
         echo 'copy file: ' . $target_file . PHP_EOL;
