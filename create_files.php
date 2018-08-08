10d9ca45c3071a6b101999c24975c3ba90b19ffa
SongLeLe <skj198568@163.com>
2018-08-08 14:47:37 +0800

    恢复文件夹和文件权限，后台任务采用切换www用户方式来执行。

--- create_files.php
+++ create_files.php
@@ -134,36 +134,6 @@ $files = [
     //模板缓存
     $document_root_dir . '/thinkphp/library/think/template/driver/File.php',
 ];
-foreach ($files as $file) {
-    //替换目录分隔符
-    $file = str_replace('/', DIRECTORY_SEPARATOR, $file);
-    if (!is_file($file)) {
-        echo $file . ' not exist' . PHP_EOL;
-        continue;
-    }
-    echo 'chown file: ' . $file . PHP_EOL;
-    $file_content = file_get_contents($file);
-    if (strpos($file_content, 'chmod 0777 %s -R') !== false) {
-        //已经处理过，不再进行处理
-        continue;
-    }
-    $file_content_array     = explode("\n", $file_content);
-    $file_content_array_new = [];
-    foreach ($file_content_array as $file_line) {
-        $file_content_array_new[] = $file_line;
-        //新增文件夹，自动更改用户组
-        if (strpos($file_line, 'mkdir') !== false) {
-            $dir = \ClassLibrary\ClString::getBetween($file_line, 'mkdir', ',', false);
-            //去除左侧（
-            $dir = \ClassLibrary\ClString::getBetween($dir, '(', '', false);
-            //cli模式下文件夹权限修改
-            $file_content_array_new[] = "IS_CLI && !IS_WIN && exec(sprintf('chmod 0777 %s -R', $dir));";
-        }
-    }
-    $file_content = implode("\n", $file_content_array_new);
-    //重新写入文件
-    file_put_contents($file, $file_content);
-}
 //系统config文件修复
 $file = $document_root_dir . '/application/config.php';
 if (is_file($file)) {
