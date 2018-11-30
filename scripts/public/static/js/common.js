f71fd2fdfaf2182ade35a26911e3bcec1a545e7b
SongLeLe <skj198568@163.com>
2018-11-30 16:31:23 +0800

    新增IE获取版本函数

--- scripts/public/static/js/common.js
+++ scripts/public/static/js/common.js
@@ -687,7 +687,7 @@ var jCommon = {
      * @returns {boolean}
      */
     empty: function (str) {
-        if (typeof(str) === "undefined" || str === 0 || str === '' || str === '0' || str === null || str === false || str === undefined || (jCommon.isString(str) && jCommon.strTrim(str).length === 0) || (jCommon.isArray(str) && str.length === 0)) {
+        if (typeof (str) === "undefined" || str === 0 || str === '' || str === '0' || str === null || str === false || str === undefined || (jCommon.isString(str) && jCommon.strTrim(str).length === 0) || (jCommon.isArray(str) && str.length === 0)) {
             return true;
         } else if (jCommon.isObject(str)) {
             var is_empty = true;
@@ -986,6 +986,34 @@ var jCommon = {
             return true;
         } else {
             return false;
+        }
+    },
+
+    /**
+     * 获取IE版本
+     * @returns {*}
+     */
+    browserGetIeVersion: function () {
+        var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+        var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器
+        var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器
+        var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+        if (isIE) {
+            var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+            reIE.test(userAgent);
+            var fIEVersion = parseFloat(RegExp["$1"]);
+            if (fIEVersion > 6) {
+                return fIEVersion;
+            } else {
+                //IE版本<=7
+                return 6;
+            }
+        } else if (isEdge) {
+            return 'edge';//edge
+        } else if (isIE11) {
+            return 11; //IE11
+        } else {
+            return -1;//不是ie浏览器
         }
     }
 
